6669722f5c25ace7fcad59182f378cc2f8f9860d
Caio Vilarouca <caiovilarouca@gmail.com>
2022-11-16 07:22:57 -0300

    Banco
    Fazendo a modelagem

---
+++ src/application/Program.java
@@ -0,0 +1,21 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		/*Classe
+		 * 	NumberConta-public
+		 * 	tipo-poupança ou corrente
+		 * 	dono
+		 * 	saldo
+		 * 	status
+		 * 
+		 * Metodos
+		 * 	abrirConta-boolean-30P-50C
+		 * 	FechaConta-não pode fecha com dinheiro na conta
+		 * 	Deposit
+		 * 	Sacar
+		 * 	PagarMensal-12.5P-50C*/
+	}
+	//Caio Vilarouca
+}
