4f613e3b4306b35ca0ad6ad5c7553168e0a25c96
Caio Vilarouca <caiovilarouca@gmail.com>
2022-11-16 07:57:22 -0300

    Tratamento de erros

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,37 @@
 package application;
-
+import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		char menu;
+		System.out.print("*-*-*-*-*-*-*- BANCO -*-*-*-*-*-*-*\n"
+				+ ""
+				+ "Abri uma conta ?\n"
+				+ "	[A] Conta comum\n"
+				+ "	[B] Conta empresarial\n"
+				+ "	[E] Exit\n"
+				+ "Digite aqui:");
+		
+		menu = scanner.next().charAt(0);
+		// Tratando erro possíveis
+		menu = menu == 'A' ?'a':menu;
+		menu = menu == 'B' ?'b':menu;
+		menu = menu == 'E' ?'e':menu;
+
+		switch (menu){
+		case 'a':
+			System.out.println("Abrindo conta comum.");
+			break;
+		case 'b':
+			System.out.println("Abrindo conta empresarial.");
+			break;
+		case 'e':
+			System.out.println("Programa fechado!");
+			break;
+		default:
+			System.out.println("ERRO, tente novamente mais tarde.");
+		}
 		/*tipo-poupança ou corrente
 
 		 * 
