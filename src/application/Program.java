100068bdbe3c87df62d5199e98cac2ff2b98a555
Caio Vilarouca <caiovilarouca@gmail.com>
2022-11-17 13:54:30 -0300

    Hud do banco

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,39 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+		
+		char menu;
+		
+		System.out.print("\n"
+				+ "│*-*-*-*-*-*-*-BANCO-*-*-*-*-*-*-*│\n"
+				+ "│---------------------------------│\n"
+				+ "│[A] Abri uma conta comum.........│\n"
+				+ "│[B] Abri uma conta para empresa..│\n"
+				+ "│[E] Fecha o aplicativo...........│\n"
+				+ "│---------------------------------│\n"
+				+ "│Opção :");
+
+		menu = scanner.next().charAt(0);
+		System.out.println();
+		if(menu == 'a' || menu == 'A') {
+			System.out.println("Abrindo conta comum");
+			
+		}else if(menu == 'b' || menu == 'B') {
+			System.out.println("Abrindo conta para empresa");
+			
+		}else if(menu == 'e' || menu == 'E') {
+			System.out.println("Programa encerrado .Volte sempre!");
+		}else {
+			System.out.println("Erro 404, tente novamente mais tarde.");
+		}
+		
 		// Cliente 
 		/*name
 		 *cpf
