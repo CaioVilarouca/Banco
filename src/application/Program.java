18801b08b7bd805363887a3dd9897c92f3384c02
Caio Vilarouca <caiovilarouca@gmail.com>
2022-11-17 07:17:06 -0300

    Modelagem do projeto
    Declaração de classe  e seus atributos

---
+++ src/application/Program.java
@@ -0,0 +1,26 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// Cliente 
+		/*ID
+		 *name
+		 *cpf
+		 *nasc
+		 *renda miníma*/
+		
+		// Conta comum
+		/*number
+		 *saldo atual
+		 *deposit
+		 *retirada com limite e juros 3.5 reais*/
+		
+		// Conta empresa
+		/*number
+		 *saldo atual
+		 *deposit
+		 *retirada sem limite e juros 5 reais*/
+	}
+
+}
