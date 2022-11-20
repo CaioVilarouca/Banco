602ffe2c6da85f4849a3c2ada9243c13587ea3b0
Caio Vilarouca <caiovilarouca@gmail.com>
2022-11-20 03:01:58 -0300

    Imprimindo no arquivo .txt

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,23 @@
 package application;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Path url = Paths.get("C:/Users/Public/Desktop/NotaFical.txt");
+		String txt = "NOTA FISCAL";
+		byte[] txtByte = txt.getBytes();
+		try {
+			Files.write(url, txtByte);
+		}catch(Exception erro){
+			System.out.println("ERRO, tente novamente mais tade!");
+		}
+		System.out.println("Fim do programa!");
+		
 		/*Notal Fical
 		 * 
 		 * Class
