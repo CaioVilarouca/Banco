5ecd6169ae095af288b14f3d13385d98ef59af61
Desenvolvedor <82101110+CaioVilarouca@users.noreply.github.com>
2023-03-13 15:31:00 -0300

    Add carrinho

--- src/application/UI.java
+++ src/application/UI.java
@@ -2,17 +2,17 @@ package application;
 
 import java.util.Scanner;
 
-import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 import entitites.Product;
+import entitites.Purchase;
 
 public class UI {
 	Scanner scanner = new Scanner(System.in);
 
 	String[] menu = { "Adicionar produto", "Valor total", "Sair" };
-	int option, optionVerification;
-	double valueProduct;
+	int option, optionVerification, amountPurchase;
+	double valueProduct, getTotalValueOfProduct;
 	boolean verification;
 
 	public void systemUI() {
@@ -33,17 +33,26 @@ public class UI {
 	}
 
 	public void systemAddProduct() {
+		
+		// Add product 
 		do {
-			
 			Product product = new Product();
 			product.setNameProduct(JOptionPane.showInputDialog(null, "Nome do Produto :", "Carrinho", 3));
 			try {
-				product.setValueProduct(valueProduct = Double
-						.parseDouble((String) JOptionPane.showInputDialog(null, "Valor do produto R$", "Preço", 3)));
+				product.setValueProduct(valueProduct = Double.parseDouble((String) JOptionPane.showInputDialog(null,"Produto [" + product.getNameProduct() + "] R$", "Preço", 3)));
+				amountPurchase = Integer.parseInt((String) JOptionPane.showInputDialog(null,"Quantidade de " + product.getNameProduct() + " R$[" + product.getValueProduct()+"] ?", "Preço", 3));
 			} catch (Exception e) {
 				// TODO: handle exception
 				JOptionPane.showMessageDialog(null, "Esse campo só aceita números.", "ERRO", 0, null);
 			}
+			
+			// Add no carrinho
+			Purchase purchase = new Purchase(amountPurchase, product); 
+
+			// Resultado de quantidade + valor do produto
+			getTotalValueOfProduct = purchase.getTotalValueOfProduct();
+			JOptionPane.showMessageDialog(null, "Preço R$["+getTotalValueOfProduct+"], Produto ["+ product.getNameProduct()+"], quatidade ["+amountPurchase+"]", "Valor final", 1);
+
 			
 			// Verificação
 			optionVerification = JOptionPane.showConfirmDialog(null, "Adicionar mais produtos ?", "Sair", 0);
@@ -64,6 +73,7 @@ public class UI {
 /*
  * try{ do { // Get de produtos System.out.printf("[ Nome do produto....: ");
  * nameProduct = scanner.nextLine();
+ * 
  * System.out.printf("[ Valor do produto....[%s] R$:", nameProduct);
  * valueProduct = scanner.nextDouble();
  * 
@@ -71,9 +81,10 @@ public class UI {
  * System.out.printf("[ Quatidade produto...[%s] : x", nameProduct);
  * amountPurchase = scanner.nextInt();
  * 
- * // Add produtos mais quantidades Purchase purchase = new Purchase
- * (amountPurchase, product); getTotalValueOfProduct =
- * purchase.getTotalValueOfProduct();
+ * // Add produtos mais quantidades 
+ * Purchase purchase = new Purchase(amountPurchase, product); 
+ * getTotalValueOfProduct = purchase.getTotalValueOfProduct();
+ * 
  * System.out.printf("[ O valor total do(a).[%s] R$%.2f \n",
  * nameProduct,getTotalValueOfProduct);
  * System.out.print("[-=*=--=*=--=*=--=*=--=*=--=*=--=*=--=*=--=*=-]\n"); // Add
